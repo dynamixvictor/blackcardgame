afa3190733d304b1b61cc4e866df0835c7475827
dynamixvictor <54214148+dynamixvictor@users.noreply.github.com>
2020-04-14 23:43:30 +0100

    Add files via upload

---
+++ card game.js
@@ -0,0 +1,21 @@
+/*
+	BlackJack
+	by Ige Victor
+*/
+
+let suits = ['Hearts', 'Clubs', 'Diamonds','Spades'];
+let values = ['Ace', 'King', 'Queen', 'Jack',
+	'Ten', 'Nine', 'Eight', 'Seven', 
+	'Six', 'Five', 'Four','Three', 'Two',];
+
+let deck = [];
+
+for (let suitIdx=0; suitIdx<suits.length; suitIdx++) {
+	for (let valueIdx = 0; valueIdx<values.length; valueIdx++) {
+		deck.push(values[valueIdx] + ' of '+ suits[suitIdx]);
+	}
+}
+for (let i=0; i<deck.length; i++){
+	console.log(deck[i]);
+}
+ console.log("You are dealt ")
